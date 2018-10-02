7e5eba000ebb88d5465e679a11c9148582541049
Alonso Muñoz <33006411+Devidences@users.noreply.github.com>
2018-10-02 09:59:29 +0200

    Char cambiados por uint8_t

--- reversi8_2018.c
+++ reversi8_2018.c
@@ -1,3 +1,5 @@
+#include "stdint.h"
+
 // Tamaño del tablero
 enum { DIM=8 };
 
@@ -17,16 +19,16 @@ FICHA_NEGRA = 2
 // candidatas: indica las posiciones a explorar
 // Se usa para no explorar todo el tablero innecesariamente
 // Sus posibles valores son NO, SI, CASILLA_OCUPADA
-const char  NO              = 0;
-const char  SI              = 1;
-const char  CASILLA_OCUPADA = 2;
+const uint8_t  NO              = 0;
+const uint8_t  SI              = 1;
+const uint8_t  CASILLA_OCUPADA = 2;
 
 /////////////////////////////////////////////////////////////////////////////
 // TABLAS AUXILIARES
 // declaramos las siguientes tablas como globales para que sean más fáciles visualizarlas en el simulador
 // __attribute__ ((aligned (8))): specifies a minimum alignment for the variable or structure field, measured in bytes, in this case 8 bytes
 
-static const char __attribute__ ((aligned (8))) tabla_valor[DIM][DIM] =
+static const uint8_t __attribute__ ((aligned (8))) tabla_valor[DIM][DIM] =
 {
     {8,2,7,3,3,7,2,8},
     {2,1,4,4,4,4,1,2},
@@ -40,8 +42,8 @@ static const char __attribute__ ((aligned (8))) tabla_valor[DIM][DIM] =
 
 
 // Tabla de direcciones. Contiene los desplazamientos de las 8 direcciones posibles
-const char vSF[DIM] = {-1,-1, 0, 1, 1, 1, 0,-1};
-const char vSC[DIM] = { 0, 1, 1, 1, 0,-1,-1,-1};
+const uint8_t vSF[DIM] = {-1,-1, 0, 1, 1, 1, 0,-1};
+const uint8_t vSC[DIM] = { 0, 1, 1, 1, 0,-1,-1,-1};
 
 //////////////////////////////////////////////////////////////////////////////////////
 // Variables globales que no deberían serlo
@@ -51,7 +53,7 @@ const char vSC[DIM] = { 0, 1, 1, 1, 0,-1,-1,-1};
 ////////////////////////////////////////////////////////////////////
 // Tablero sin inicializar
 ////////////////////////////////////////////////////////////////////
-signed char __attribute__ ((aligned (8))) tablero[DIM][DIM] = {
+uint8_t __attribute__ ((aligned (8))) tablero[DIM][DIM] = {
 	        {CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA},
 	        {CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA},
 	        {CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA,CASILLA_VACIA},
@@ -66,16 +68,16 @@ signed char __attribute__ ((aligned (8))) tablero[DIM][DIM] = {
      // VARIABLES PARA INTERACCIONAR CON LA ENTRADA SALIDA
      // Pregunta: ¿hay que hacer algo con ellas para que esto funcione bien?
      // (por ejemplo añadir alguna palabra clave para garantizar que la sincronización a través de esa variable funcione)
-  char fila=0, columna=0, ready = 0;
+  uint8_t fila=0, columna=0, ready = 0;
 
 
 
-extern int patron_volteo_arm_c(char tablero[][8], int *longitud,char f, char c, char SF, char SC, char color);
+extern int patron_volteo_arm_c(uint8_t tablero[][8], int *longitud,uint8_t f, uint8_t c, uint8_t SF, uint8_t SC, uint8_t color);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 0 indica CASILLA_VACIA, 1 indica FICHA_BLANCA y 2 indica FICHA_NEGRA
 // pone el tablero a cero y luego coloca las fichas centrales.
-void init_table(char tablero[][DIM], char candidatas[][DIM])
+void init_table(uint8_t tablero[][DIM], uint8_t candidatas[][DIM])
 {
     int i, j;
 
@@ -132,7 +134,7 @@ void init_table(char tablero[][DIM], char candidatas[][DIM])
 // CUIDADO: si el compilador coloca esta variable en un registro, no funcionará.
 // Hay que definirla como "volatile" para forzar a que antes de cada uso la cargue de memoria
 
-void esperar_mov(char *ready)
+void esperar_mov(uint8_t *ready)
 {
     while (*ready == 0) {};  // bucle de espera de respuestas hasta que el se modifique el valor de ready (hay que hacerlo manualmente)
 
@@ -159,9 +161,9 @@ void esperar_mov(char *ready)
 // Además informa si la posición es válida y contiene alguna ficha.
 // Esto lo hace por referencia (en *posicion_valida)
 // Si devuelve un 0 no es válida o está vacia.
-char ficha_valida(char tablero[][DIM], char f, char c, int *posicion_valida)
+uint8_t ficha_valida(uint8_t tablero[][DIM], uint8_t f, uint8_t c, int *posicion_valida)
 {
-    char ficha;
+    uint8_t ficha;
 
     // ficha = tablero[f][c];
     // no puede accederse a tablero[f][c]
@@ -188,11 +190,11 @@ char ficha_valida(char tablero[][DIM], char f, char c, int *posicion_valida)
 // la función devuelve PATRON_ENCONTRADO (1) si encuentra patrón y NO_HAY_PATRON (0) en caso contrario
 // FA y CA son la fila y columna a analizar
 // longitud es un parámetro por referencia. Sirve para saber la longitud del patrón que se está analizando. Se usa para saber cuantas fichas habría que voltear
-int patron_volteo(char tablero[][DIM], int *longitud, char FA, char CA, char SF, char SC, char color)
+int patron_volteo(uint8_t tablero[][DIM], int *longitud, uint8_t FA, uint8_t CA, uint8_t SF, uint8_t SC, uint8_t color)
 {
     int posicion_valida; // indica si la posición es valida y contiene una ficha de algún jugador
     int patron; //indica si se ha encontrado un patrón o no
-    char casilla;   // casilla es la casilla que se lee del tablero
+    uint8_t casilla;   // casilla es la casilla que se lee del tablero
     FA = FA + SF;
     CA = CA + SC;
     casilla = ficha_valida(tablero, FA, CA, &posicion_valida);
@@ -232,7 +234,7 @@ int patron_volteo(char tablero[][DIM], int *longitud, char FA, char CA, char SF,
 // SF y SC son las cantidades a sumar para movernos en la dirección que toque
 // color indica el color de la pieza que se acaba de colocar
 // FA y CA son la fila y columna a analizar
-void voltear(char tablero[][DIM], char FA, char CA, char SF, char SC, int n, char color)
+void voltear(uint8_t tablero[][DIM], uint8_t FA, uint8_t CA, uint8_t SF, uint8_t SC, int n, uint8_t color)
 {
     int i;
 
@@ -247,11 +249,11 @@ void voltear(char tablero[][DIM], char FA, char CA, char SF, char SC, int n, cha
 // comprueba si hay que actualizar alguna ficha
 // no comprueba que el movimiento realizado sea válido
 // f y c son la fila y columna a analizar
-// char vSF[DIM] = {-1,-1, 0, 1, 1, 1, 0,-1};
-// char vSC[DIM] = { 0, 1, 1, 1, 0,-1,-1,-1};
-int actualizar_tablero(char tablero[][DIM], char f, char c, char color)
+// uint8_t vSF[DIM] = {-1,-1, 0, 1, 1, 1, 0,-1};
+// uint8_t vSC[DIM] = { 0, 1, 1, 1, 0,-1,-1,-1};
+int actualizar_tablero(uint8_t tablero[][DIM], uint8_t f, uint8_t c, uint8_t color)
 {
-    char SF, SC; // cantidades a sumar para movernos en la dirección que toque
+    uint8_t SF, SC; // cantidades a sumar para movernos en la dirección que toque
     int i, flip, patron;
 
     for (i = 0; i < DIM; i++) // 0 es Norte, 1 NE, 2 E ...
@@ -280,14 +282,14 @@ int actualizar_tablero(char tablero[][DIM], char f, char c, char color)
 // NO    0
 // SI    1
 // CASILLA_OCUPADA 2
-int elegir_mov(char candidatas[][DIM], char tablero[][DIM], char *f, char *c)
+int elegir_mov(uint8_t candidatas[][DIM], uint8_t tablero[][DIM], uint8_t *f, uint8_t *c)
 {
     int i, j, k, found;
     int mf = -1; // almacena la fila del mejor movimiento encontrado
     int mc;      // almacena la columna del mejor movimiento encontrado
-    char mejor = 0; // almacena el mejor valor encontrado
+    uint8_t mejor = 0; // almacena el mejor valor encontrado
     int patron, longitud;
-    char SF, SC; // cantidades a sumar para movernos en la dirección que toque
+    uint8_t SF, SC; // cantidades a sumar para movernos en la dirección que toque
 
     // Recorremos todo el tablero comprobando dónde podemos mover
     // Comparamos la puntuación de los movimientos encontrados y nos quedamos con el mejor
@@ -330,15 +332,15 @@ int elegir_mov(char candidatas[][DIM], char tablero[][DIM], char *f, char *c)
             }
         }
     }
-    *f = (char) mf;
-    *c = (char) mc;
+    *f = (uint8_t) mf;
+    *c = (uint8_t) mc;
     // si no se ha encontrado una posición válida devuelve -1
     return mf;
 }
 ////////////////////////////////////////////////////////////////////////////////
 // Cuenta el número de fichas de cada color.
 // Los guarda en la dirección b (blancas) y n (negras)
-void contar(char tablero[][DIM], int *b, int *n)
+void contar(uint8_t tablero[][DIM], int *b, int *n)
 {
     int i,j;
 
@@ -362,7 +364,7 @@ void contar(char tablero[][DIM], int *b, int *n)
     }
 }
 
-void actualizar_candidatas(char candidatas[][DIM], char f, char c)
+void actualizar_candidatas(uint8_t candidatas[][DIM], uint8_t f, uint8_t c)
 {
     // donde ya se ha colocado no se puede volver a colocar
     // En las posiciones alrededor sí
@@ -416,7 +418,7 @@ void reversi8()
 	 // Tablero candidatas: se usa para no explorar todas las posiciones del tablero
 	// sólo se exploran las que están alrededor de las fichas colocadas
 	 ////////////////////////////////////////////////////////////////////
-	char __attribute__ ((aligned (8))) candidatas[DIM][DIM] =
+	uint8_t __attribute__ ((aligned (8))) candidatas[DIM][DIM] =
     {
         {NO,NO,NO,NO,NO,NO,NO,NO},
         {NO,NO,NO,NO,NO,NO,NO,NO},
@@ -435,7 +437,7 @@ void reversi8()
     int fin = 0;  // fin vale 1 si el humano no ha podido mover
                   // (ha introducido un valor de movimiento con algún 8)
                   // y luego la máquina tampoco puede
-    char f, c;    // fila y columna elegidas por la máquina para su movimiento
+    uint8_t f, c;    // fila y columna elegidas por la máquina para su movimiento
 
     init_table(tablero, candidatas);
     while (fin == 0)
